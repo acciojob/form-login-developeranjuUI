acf8c9f5f89ee6bcd9fafae7151a2b76e911ea30
developeranjuUI <116105555+developeranjuUI@users.noreply.github.com>
2025-03-31 14:57:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
     //Write your code here
-
+	let firstname = document.getElementById("fname").value.trim();
+	let lastname = document.getElementById('lname').value.trim();
+	alert(`${firstname} ${lastname}`);
 }
+document.getElementById('form1').addEventListener('submit',getFormvalue);
